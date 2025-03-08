3e2aa1029e9f4f5163d0490b55e976ee3f14f923
Abhinav Singh <abhisingh622000@gmail.com>
2025-03-08 20:53:29 +0530

    small changes1

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors(
     {
-        origin: 'http://localhost:5000',
+        origin: 'https://xzayogan-dashboard-api.vercel.app/',
         credentials: true,
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
     }
