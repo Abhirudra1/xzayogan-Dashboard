61ae8309c51859c2cc7dbd177662155480e79cc6
Abhinav Singh <abhisingh622000@gmail.com>
2025-03-08 20:47:55 +0530

    small changes

--- server/index.js
+++ server/index.js
@@ -9,7 +9,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors(
+    {
+        origin: 'http://localhost:5000',
+        credentials: true,
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    }
+));
 app.use(express.json());
 
 // Google OAuth Client
